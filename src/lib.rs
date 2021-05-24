8997dcc09da12df6c5fb265d16025de33d26a4e4
cutsea110 <cutsea110@gmail.com>
2021-05-25 00:38:13 +0900

    buggy

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,55 @@
+pub mod post {
+    pub trait PostDao {
+        type NewPost;
+        type Post;
+        type PostId;
+
+        fn create(&mut self, post: Self::NewPost) -> Option<Self::PostId>;
+        fn list_draft(&self) -> Vec<Self::Post>;
+        fn list_published(&self) -> Vec<Self::Post>;
+        fn get_by_id(&self, id: Self::PostId) -> Option<Self::Post>;
+        fn publish(&mut self, id: Self::PostId) -> bool;
+    }
+    pub trait HavePostDao {
+        type PostDao: PostDao;
+        fn post_dao(&self) -> Self::PostDao;
+    }
+    pub trait PostService: HavePostDao {
+        fn write_new(
+            &self,
+            post: <<Self as HavePostDao>::PostDao as PostDao>::NewPost,
+        ) -> Option<<<Self as HavePostDao>::PostDao as PostDao>::PostId> {
+            self.post_dao().create(post)
+        }
+
+        fn list_draft(&self) -> Vec<<<Self as HavePostDao>::PostDao as PostDao>::Post> {
+            self.post_dao().list_draft()
+        }
+
+        fn list_published(&self) -> Vec<<<Self as HavePostDao>::PostDao as PostDao>::Post> {
+            self.post_dao().list_published()
+        }
+
+        fn get_post_by_id(
+            &self,
+            id: <<Self as HavePostDao>::PostDao as PostDao>::PostId,
+        ) -> Option<<<Self as HavePostDao>::PostDao as PostDao>::Post> {
+            self.post_dao().get_by_id(id)
+        }
+
+        fn publish(&self, id: <<Self as HavePostDao>::PostDao as PostDao>::PostId) -> bool {
+            self.post_dao().publish(id)
+        }
+    }
+
+    impl<T: HavePostDao> PostService for T {}
+
+    pub trait HavePostService {
+        type PostService: PostService;
+        fn post_service(&self) -> Self::PostService;
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
---
+++ src/main.rs
@@ -0,0 +1,123 @@
+extern crate todo;
+
+pub mod mock {
+    pub mod dao {
+
+        use todo::post::*;
+
+        #[derive(Debug)]
+        pub struct MockNewPost {
+            pub memo: String,
+        }
+        #[derive(Debug, Clone)]
+        pub struct MockPost {
+            pub id: u32,
+            pub memo: String,
+        }
+
+        #[derive(Debug, Clone)]
+        pub struct MockDao {
+            id_counter: u32,
+            drafts: Vec<MockPost>,
+            published: Vec<MockPost>,
+        }
+
+        impl MockDao {
+            pub fn new() -> Self {
+                MockDao {
+                    id_counter: 0,
+                    drafts: vec![],
+                    published: vec![],
+                }
+            }
+        }
+
+        impl PostDao for MockDao {
+            type NewPost = MockNewPost;
+            type Post = MockPost;
+            type PostId = u32;
+
+            fn create(&mut self, post: MockNewPost) -> Option<u32> {
+                self.id_counter = self.id_counter + 1;
+                self.drafts.push(MockPost {
+                    id: self.id_counter,
+                    memo: post.memo,
+                });
+                println!("CREATED: {:#?}", self);
+                Some(self.id_counter)
+            }
+            fn list_draft(&self) -> Vec<MockPost> {
+                self.drafts.clone()
+            }
+            fn list_published(&self) -> Vec<MockPost> {
+                self.published.clone()
+            }
+            fn get_by_id(&self, id: Self::PostId) -> Option<MockPost> {
+                self.drafts.clone().into_iter().find(|p| p.id == id).or(self
+                    .published
+                    .clone()
+                    .into_iter()
+                    .find(|p| p.id == id))
+            }
+            fn publish(&mut self, id: u32) -> bool {
+                for (i, v) in self.drafts.clone().into_iter().enumerate() {
+                    if v.id == id {
+                        self.drafts.remove(i);
+                        self.published.push(v);
+                        return true;
+                    }
+                }
+                false
+            }
+        }
+        impl HavePostDao for MockDao {
+            type PostDao = MockDao;
+            fn post_dao(&self) -> MockDao {
+                (*self).clone()
+            }
+        }
+    }
+
+    pub mod service {
+        pub use super::dao::*;
+        pub use todo::post::*;
+
+        #[derive(Debug, Clone)]
+        pub struct MockService {
+            post_dao: MockDao,
+        }
+        impl MockService {
+            pub fn new() -> Self {
+                MockService {
+                    post_dao: MockDao::new(),
+                }
+            }
+        }
+        impl HavePostDao for MockService {
+            type PostDao = MockDao;
+            fn post_dao(&self) -> MockDao {
+                self.post_dao.clone()
+            }
+        }
+        impl HavePostService for MockService {
+            type PostService = Self;
+            fn post_service(&self) -> Self {
+                (*self).clone()
+            }
+        }
+    }
+}
+
+use mock::service::*;
+
+fn main() {
+    let svc = MockService::new();
+    println!("{:#?}", svc);
+
+    let pid = svc.write_new(MockNewPost {
+        memo: "Hello".to_string(),
+    });
+    println!("{:#?}", pid);
+
+    println!("{:#?}", svc);
+}
